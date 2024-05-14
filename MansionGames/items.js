08566fd1195d03d80ad02f47fc29ab2106e89d2c
EmberGamingStudios <157764375+EmberGamingStudios@users.noreply.github.com>
2024-05-14 00:05:34 -0500

    Add files via upload

---
+++ MansionGames/items.js
@@ -0,0 +1,102 @@
+var weapons = [
+    "landmine",
+    "bow",
+    "melee",
+    "axe"
+];
+
+var melee = [
+    "sword",
+    "knife",
+    "baseball bat",
+    "stick"
+];
+
+var itemsToFind = [
+    "food",
+    "clean water",
+    "tent",
+    "diamond pickaxe"
+];
+
+var itemsThatGivePoints = [
+    "food",
+    "clean water"
+];
+
+var causeAndEffect = [
+    "food", "eats food",
+    "clean water", "drinks clean water",
+    "tent", "constructs a tent",
+    "diamond pickaxe", "mines diamond ore"
+];
+
+var killCauseAndEffect = [
+    "landmine", "random mine",
+    "bow", "shoots",
+    "melee", "kills"
+];
+
+var shelter = [
+    "cave",
+    "tree",
+    "box",
+    "tent"
+];
+
+var cornucopia = [
+    "run",
+    "supply",
+    "weapon",
+    "battle"
+];
+
+var sponsor = [
+    "empty box",
+    "supplies",
+    "clean water",
+    "tent"
+];
+
+var misc = [
+    "wanders around",
+    "picks flowers",
+    "talks to",
+    "sings"
+];
+
+var injury = [
+    "gets a cut",
+    "gets a broken bone",
+    "falls from a tree"
+];
+
+var injEffect = [
+    "bow", "injures",
+    "melee", "injures",
+    "arrow", "gets hit by an arrow shot by"
+];
+
+var variousDeaths = [
+    "strangles",
+    "dies of disease",
+    "falls from a tree"
+];
+
+var doubleDeath = [
+    "strangles",
+    "gets assasinated by",
+    "fatally roundhouse kicks"
+];
+
+var options = [
+    "weapons",
+    "items",
+    "use",
+    "shelter",
+    "misc",
+    "injury",
+    "death"
+];
+
+var vowels = ["a", "e", "i", "o", "u"];
